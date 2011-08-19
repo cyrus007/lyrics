cc2dff165741ddc8b7956ee671acd292849d5a84
Swapan <swapan@yahoo.com>
2011-08-19 12:20:31 -0500

    Refactor GET routines into one

--- lyrics.rb
+++ lyrics.rb
@@ -1,18 +1,17 @@
 # lyrics.rb
-%w[ net/http uri rexml/document rexml/xpath sinatra].each{ |gem| require gem }
+%w[ net/http uri rexml/document rexml/xpath json sinatra].each{ |gem| require gem }
 require './hitrans'
 include HITRANS
 
 class Lyrics
   attr_accessor :lyricsurl, :showurl, :page_url, :title, :composer, :lyricist, :singer, :album, :year, :lyrics
-  ERRORMSG  = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<lyrics>Server is not responding to lyrics fetch instruction.</lyrics>"
+  ERRORMSG  = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<lyrics>Sorry, server is not responding to lyrics fetch instruction.</lyrics>"
 
   def initialize (host, port)
     self.title = self.lyrics = self.composer = self.lyricist = self.singer = self.album = self.year = ""
   end
 
-  def format(items)
-    if items.empty?
+  def format_show
       yield
       output = "<table border=\"0\" cellpadding=\"0\" style=\"color:#000;font-size:.8em\">\n<tr><td>Song: {title} </td></tr>\n<tr><td>From: {album} {year} </td></tr>\n<tr><td>Music Director: {composer} </td></tr>\n<tr><td>Lyrics: {lyricist}</td></tr>\n<tr><td>Singers: {singer} </td></tr> </table>\n<p style=\"color:#000;font-size:.8em\"> {lyrics} </p>\n<hr/><div style=\"margin-left:1em\">{links}</div>"
 
@@ -26,9 +25,10 @@ class Lyrics
       output.sub!( "{lyrics}", self.lyrics )
       self.year ? output.sub!( "{year}", ' @ ' + self.year ) : output.sub!( "{year}", '' )
       return output
-    else
-      return "<suggestions page_url=\"#{self.page_url}\">\n" + items.first.map { |s| "<suggestion title=\"#{s[:title]}\" url=\"#{self.showurl}#{s[:id]}\" />\n" }.join + "</suggestions>"
-    end
+  end
+
+  def format(items)
+      "<suggestions page_url=\"#{self.page_url}\">\n" + items.map { |s| "<suggestion title=\"#{s[:title]}\" url=\"#{self.showurl}#{s[:id]}\" />\n" }.join + "</suggestions>"
   end
 end
 
@@ -37,6 +37,17 @@ class LI < Lyrics
     super
     self.lyricsurl = "http://alpha.lyricsindia.net/songs/"
     self.showurl = "http://" + host + ":" + port.to_s + "/li/show/"
+    self.page_url = ""
+  end
+
+  def fetch_search(title)
+    fullurl = self.lyricsurl + "search?txt=#{title}&format=xml"
+    Net::HTTP.get(URI.parse(fullurl))
+  end
+
+  def fetch_show(song_no)
+    fullurl = self.lyricsurl + url + ".xml"
+    Net::HTTP.get(URI.parse(fullurl))
   end
 
   def extract_songs(response)
@@ -69,8 +80,8 @@ class LI < Lyrics
     xmlDoc.elements.each('/song/movie/year') { |child| self.year = child.text }
   end
 
-  def format(*items)
-    super(items).sub!( "{links}", "<a href='http://lyricsindia.net/'>LyricsIndia</a>" )
+  def format_show
+    super.sub!( "{links}", "<a href='http://lyricsindia.net/'>LyricsIndia</a>" )
   end
 end
 class GIIT < Lyrics
@@ -81,6 +92,18 @@ class GIIT < Lyrics
     self.lyricsurl = "http://giitaayan.com/"
     self.cisburl = "http://thaxi.hsc.usc.edu/rmim/giitaayan/"
     self.showurl = "http://" + host + ":" + port.to_s + "/gi/show/"
+    self.page_url = ""
+  end
+
+  def fetch_search(title)
+    fullurl = self.lyricsurl + "search.asp?browse=stitle&s=#{title}&submit=search"
+    Net::HTTP.get(URI.parse(fullurl))
+  end
+
+  def fetch_show(song_no)
+    song_no = (song_no[0..3] == 'cisb' ? 'cisb/'+song_no[4..song_no.length] : 'files/'+song_no)
+    fullurl = self.cisburl + song_no + ".isb"
+    Net::HTTP.get(URI.parse(fullurl))
   end
 
   def extract_songs(response)
@@ -97,9 +120,13 @@ class GIIT < Lyrics
       while i < count+2 do
         cells = rows[i-page*50].split("<td>")
         film = cells[4].match(/">[^<]*[\/]*<\//).to_s
+        artist = cells[7].match(/">[^<]*[\/]*<\//).to_s
         src, song_no = cells[1].match(/http:\/\/thaxi\.hsc\.usc\.edu\/rmim\/giitaayan\/(.*).isb/) { |m| $1 }.split('/')
-        song_no = (src == 'cisb' ? 'cisb' + song_no : song_no)
-        suggestion << { :title => '<b>' + film[2..film.length-3] + '</b> : ' + HITRANS::convertHindi(cells[3][0...cells[3].index(/&nbsp;/)]), :id => song_no }
+        song_no = (src == 'cisb' ? 'cisb' + song_no : song_no)       #src can be either cisb or rmim
+        suggestion << { :title => HITRANS::convertHindi(cells[3][0...cells[3].index(/&nbsp;/)]),
+                        :id => song_no,
+                        :film => film[2..film.length-3],
+                        :artist => artist[2..artist.length-3] }
         i += 1
         throw :done if i == 52
       end
@@ -116,125 +143,82 @@ class GIIT < Lyrics
       self.lyricist = response.match( /\\lyrics\{([-a-zA-Z_.,()\s\/]*)\}%/ ) { $1 }.to_s
       response.gsub!(/##/, "$")
       self.lyrics = response.match( /#indian([^#]*)#endindian/ ) { $1 }.to_s
-      self.lyrics = HITRANS::convertHindi( self.lyrics.gsub!( /%/, '' ) )
+#     self.lyrics.gsub!( /%/, '' )
+      self.lyrics = HITRANS::convertHindi( self.lyrics )
   end
 
-  def format(*items)
-    super(items).sub( "{links}", "<a href='http://giitaayan.com/'>Giitaayan</a>" )
+  def format_show
+    super.sub( "{links}", "<a href='http://giitaayan.com/'>Giitaayan</a>" )
   end
 
+end
+class XBMC < GIIT
+  def initialize(host, port)
+    super
+#   self.lyricsurl = "http://192.168.1.150/itrans/"
+    self.showurl = "http://" + host + ":" + port.to_s + "/cu/show/"
+    self.page_url = ""
+  end
+
+  def format_show
+      yield
+      output = "<lyrics>#{self.lyrics}</lyrics>"
+      output.gsub!( /^\s[\s]+/, "" )       #remove multiple blank lines
+#     output.gsub!( /\n/, "\<br /\>" )     #replace line breaks with <br/>
+  end
+
+  def format(items, artist='', film='')
+      suggestions = items.delete_if do |s|
+          (artist && !s[:artist].upcase.include?(artist)) || (film && !s[:film].upcase.include?(film))
+      end
+      suggestions.to_json
+  end
 end
 
 get '/' do
   'Welcome to Hindi lyrics fetcher ...'
 end
 
-get '/li/search/:title', :provides => 'text' do |t|
+get '/search/:use?*' do
 
-  return "Song title is empty." if t.empty?
+  using = params[:use] if params[:use]
+  title = URI.escape( params[:title] ) if params[:title]
+  title = URI.escape( params[:Title] ) if (!title) && params[:Title]
+  artist = params[:artist].upcase if params[:artist]
+  artist = params[:Artist].upcase if (!artist) && params[:Artist]
+  film = params[:film].upcase if params[:film]
+  film = params[:Film].upcase if (!film) && params[:Film]
+  return Lyrics:ERRORMSG if using.empty? || title.empty?
 
-  lyrics = LI.new(request.host, request.port)
-  lyrics.page_url = ""
-  title = URI.escape(t)
-  content = Net::HTTP.get(URI.parse(lyrics.lyricsurl + "search?txt=#{title}&format=xml"))
+  if using == 'li' then lyrics = LI.new(request.host, request.port) end
+  if using == 'gi' then lyrics = GIIT.new(request.host, request.port) end
+  if using == 'cu' then lyrics = XBMC.new(request.host, request.port) end
+
+  content = lyrics.fetch_search(title)
   return Lyrics::ERRORMSG if( content.empty? || content.include?( "Sorry" ) )
+
   suggestion, song_no = lyrics.extract_songs(content)
   if suggestion.size == 1
-p    url = lyrics.lyricsurl + song_no + ".xml"
-    content = Net::HTTP.get(URI.parse(url))
-    return Lyrics::ERRORMSG if( content.empty? || content.include?( "Sorry, no" ) )
-    return lyrics.format { lyrics.parse(content) }
+    content = lyrics.fetch_show(song_no)
+    return Lyrics::ERRORMSG if( content.empty? || content.include?( "Sorry" ) )
+    return lyrics.format_show { lyrics.parse(content) }
   else
-    return lyrics.format(suggestion)
+    return lyrics.format(suggestion, artist, film)
   end
 end
 
-get '/li/show/:id', :provides => 'text' do |id|
+get '/show/:use/:id', :provides => 'text' do |using, id|
 
-  return "Song id is empty." if id.empty?
+  return Lyrics::ERRMSG if using.empty? || id.empty?
 
-  lyrics = LI.new(request.host, request.port)
+  if using == 'li' then lyrics = LI.new(request.host, request.port) end
+  if using == 'gi' then lyrics = GIIT.new(request.host, request.port) end
+  if using == 'cu' then lyrics = XBMC.new(request.host, request.port) end
+
   song_id = URI.escape(id)
-  content = Net::HTTP.get(URI.parse(lyrics.lyricsurl + song_id + ".xml"))
-  if content.empty? || content.include?( "Sorry, no" )
-      return Lyrics::ERRORMSG
-  end
-  return lyrics.format { lyrics.parse(content) }
-end
-
-get '/gi/search/:title', :provides => 'text' do |t|
-
-  return "Song title is empty." if t.empty?
-
-  lyrics = GIIT.new(request.host, request.port)
-  lyrics.page_url = ""
-  title = URI.escape(t)
-  content = Net::HTTP.get(URI.parse(lyrics.lyricsurl + "search.asp?browse=stitle&s=#{title}&submit=search"))
-  return Lyrics::ERRORMSG if( content.empty? || content.include?( "Sorry, no" ) )
-  suggestion, song_no = lyrics.extract_songs(content)
-  if suggestion.size == 1
-    song_no = (song_no[0..3] == 'cisb' ? 'cisb/'+song_no[4..song_no.length] : 'files/'+song_no)
-p    url = lyrics.cisburl + song_no + ".isb"
-    content = Net::HTTP.get(URI.parse(url))
-    return Lyrics::ERRORMSG if( content.empty? || content.include?( "Sorry, no" ) )
-    return lyrics.format { lyrics.parse(content) }
-  else
-    return lyrics.format(suggestion)
-  end
-end
-
-get '/gi/show/:id', :provides => 'text' do |id|
-
-  return "Song id is empty." if id.empty?
-
-  lyrics = GIIT.new(request.host, request.port)
-  song_id = URI.escape( song_id = (id[0..3] == 'cisb' ? 'cisb/'+id[4..id.length] : 'files/'+id))
-  content = Net::HTTP.get(URI.parse(lyrics.cisburl + song_id + ".isb"))
-  if content.empty? || content.include?( "Sorry, no" )
-      return Lyrics::ERRORMSG
-  end
-  return lyrics.format { lyrics.parse(content) }
-end
-
-get '/cu/search/:title' do |t|
-
-#  return "Please provide the title." if t.empty?
-
-  title = URI.escape(t)
-  url = "http://192.168.1.150/itrans/" + "search?txt=#{title}&format=xml"
-p url
-  content = Net::HTTP.get(URI.parse(url))
-  if content.empty? || content.include?( "Sorry, no" )
-      return
-  end
-
-#p content
-    xmlDoc = REXML::Document.new(content.to_s.gsub("\n", "")).root
-    nodes = REXML::XPath.match(xmlDoc, '/songs/song')
-    if nodes.size == 1
-      song_id = title = lyrics = ""
-      nodes[0].elements.each('id') { |child| song_id = child.text }
-      url = "http://192.168.1.150/itrans/" + song_id + '.xml'
-p url
-      content = Net::HTTP.get(URI.parse(url))
-      begin
-        xmlDoc = REXML::Document.new(content).root
-        xmlDoc.elements.each('/song/unititle') { |child| title = child.text.gsub!(/\n/, "") }
-        xmlDoc.elements.each('/song/unidata') { |child| lyrics = child.text }
-      rescue
-        
-      end
-      return "<lyrics>\n" + lyrics + "</lyrics>"
-    else
-      self.page_url = suggestion = output = ""
-      REXML::XPath.match(xmlDoc, '/songs/song').each do |node|
-        song_no = title = ""
-        node.elements.each('id') { |child| song_no = child.text }
-        node.elements.each('title') { |child| title = child.text }
-        suggestion += "<suggestion>#{title}</suggestion>\n"
-      end
-      return "<suggestions>\n" + suggestion + "</suggestions>"
-    end
+  content = lyrics.fetch_show(song_id)
+  return Lyrics::ERRORMSG if( content.empty? || content.include?( "Sorry" ) )
+  return lyrics.format_show { lyrics.parse(content) }
 end
 
 error do
